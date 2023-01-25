3a71725915514b5d23f09e11427a2479e38ee100
Gabriel Bessi <tecbessi@gmail.com>
2023-01-25 13:31:17 -0500

    feat: Primeira parte da entrega finalizada

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from .management.product_handler import get_product_by_id
+from management import get_product_by_id, get_products_by_type
+from menu import products
+
+
+def looping_Id():
+    for id_products in products:
+        get_product_by_id(id_products["_id"])
+
+
+def looping_type():
+    for type_products in products:
+        get_products_by_type(type_products["type"])
+
 
 if __name__ == "__main__":
     # Seus prints de teste aqui
-    print(get_product_by_id)
+    print(looping_Id())
+    print(looping_type())
     ...
--- management/product_handler.py
+++ management/product_handler.py
@@ -1,6 +1,23 @@
 from menu import products
 
 
-def get_product_by_id(id):
-    for i in products:
-        print(i)
+def get_product_by_id(id: int):
+    if type(id) != int:
+        return {}
+
+    for id_products in products:
+        if id_products["_id"] == id:
+            return id_products
+    return {}
+
+
+def get_products_by_type(prod_type: str):
+    if type(prod_type) != str:
+        return []
+
+    list_products = []
+    for type_products in products:
+        if type_products["type"] == prod_type:
+            list_products.append(type_products)
+
+    return list_products
